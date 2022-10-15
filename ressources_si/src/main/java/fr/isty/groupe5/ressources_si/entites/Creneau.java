5874094109e5d04217bdeefafad9cbf147acba9a
Jihad Ghannoum <jihad@paul.home>
2022-10-15 13:56:01 +0200

    Add Creneau

--- ressources_si/src/main/java/fr/isty/groupe5/ressources_si/entites/Creneau.java
+++ ressources_si/src/main/java/fr/isty/groupe5/ressources_si/entites/Creneau.java
@@ -1,5 +1,47 @@
 package fr.isty.groupe5.ressources_si.entites;
 
-public class Creneau {
+import java.io.Serializable;
+
+
+public class Creneau implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private long identifinat;
+	private String debut;
+	private String fin;
+	
+	@SuppressWarnings("unused") // Constructeur par défaut interdit
+    private Creneau() {
+        throw new RuntimeException();
+    }
+	
+	public Creneau(String debut, String fin) {
+		
+        if(debut == null || fin == null) {
+            throw new AssertionError();
+        }
+        
+        this.debut = debut;
+        this.fin = fin;
+    }
+	
+	
+	private long getIdentifiant() {
+		return identifiant;
+	}
+	
+	private String getDebut() {
+		return debut;
+	}
+	
+	private String getFin() {
+		return fin;
+	}
+	
+	@Override
+	public String toString() {
+		return "Creneau [identifiant=" + identifiant + ", de " + debut + " jusqu'à " + fin + "]";
+	}
 
 }
---
+++ ressources_si/src/main/java/fr/isty/groupe5/ressources_si/entites/CreneauRepo.java
@@ -0,0 +1,39 @@
+package fr.isty.groupe5.ressources_si.entites;
+
+import fr.isty.groupe5.ressources_si.persistance.Sauvegarde;
+import java.util.ArrayList;
+
+public class CreneauRepo {
+	
+    private static final String NOM_FICHIER = "creneaux.dat";
+    private static ArrayList<Creneau> repo = new ArrayList<>();
+
+    public static ArrayList<Creneau> repo() {
+        return repo;
+    }
+
+    public static void ajouter(Creneau creneau) {
+        repo.add(creneau);
+    }
+
+    public static void supprimer(Creneau creneau) {
+        repo.remove(creneau);
+    }
+
+    public static void raz() {
+        repo = new ArrayList<>();
+    }
+
+    public static void sauvegarder() {
+        Sauvegarde.enregistrer(repo, NOM_FICHIER);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static void charger() {
+        repo = (ArrayList<creneau>) Sauvegarde.charger(NOM_FICHIER);
+    }
+
+    public static void supprimerFichier() {
+        Sauvegarde.supprimer(NOM_FICHIER);
+    }
+}
