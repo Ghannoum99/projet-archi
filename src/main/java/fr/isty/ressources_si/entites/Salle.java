e328534818ce3fe4c4546f68ec9a988a62d6defd
Alex CORBEIL <20360331+xelaco@users.noreply.github.com>
2022-10-15 11:40:35 +0200

    Début de Salle et suppression .project

--- src/main/java/fr/isty/ressources_si/entites/Salle.java
+++ src/main/java/fr/isty/ressources_si/entites/Salle.java
@@ -2,4 +2,19 @@ package fr.isty.ressources_si.entites;
 
 public class Salle {
 
+	private String nom;
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	@Override
+	public String toString() {
+		return "Salle [nom=" + nom + "]";
+	}
+
 }
